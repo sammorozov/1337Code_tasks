99844ddcdc60ba9599b7d41ed8eb40da3a9e996c
sam <an.sem2001@yandex.ru>
2024-03-11 03:11:39 +0300

    add another task

---
+++ 500. Keyboard Row.js
@@ -0,0 +1,17 @@
+    let words = ['qwe', 'asd', 'aq']
+    
+    let fst = 'qwertyuiop';
+    let snd = 'asdfghjkl';
+    let thrd = 'zxcvbnm';
+    let out = [];
+
+    for (let i = 0; i < words.lendth; i++){
+
+        for (let j = 0; j < words[i].length;  j ++){
+
+            
+
+        }
+
+
+    }
