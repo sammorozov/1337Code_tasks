a779302a53a1ae2b7161e5ccbf405f5e9e0a36ac
sam <an.sem2001@yandex.ru>
2024-03-11 04:06:35 +0300

    add 705

--- 705. Design HashSet.py
+++ 705. Design HashSet.py
@@ -1,16 +1,26 @@
 class MyHashSet:
 
     def __init__(self):
-        
+        self.arr = []
 
     def add(self, key: int) -> None:
-        
+
+        if key not in self.arr:
+            self.arr.append(key)
+        else:
+            pass
 
     def remove(self, key: int) -> None:
-        
+        if key in self.arr:
+            self.arr.remove(key)
+        else:
+            pass
 
     def contains(self, key: int) -> bool:
-        
+        if key in self.arr:
+            return True
+        else:
+            return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
